6e686a656f9f5e4b1dd0ca743ce030eca7157a00
blindflier <jiesong.yuan@gmail.com>
2015-09-06 22:45:06 +0800

    n

--- src/app/services/currentUser.service.js
+++ src/app/services/currentUser.service.js
@@ -33,7 +33,6 @@ angular.module('bodhiStudentAui')
         user.logout = function() {
             StorageService.remove('token');
             token = null;
-            window.location = "/";
         };
         user.permissions = function() {
             return (token && token.student) ?
